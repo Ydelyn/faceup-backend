b8682d7679fc8cc175cb93ea852c09f8cd75acce
Lorris <colini.lorris@orange.fr>
2023-06-07 11:55:43 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+const uniqid = require('uniqid');
+
+
+router.post('/upload', async (req, res) => {
+    const photoPath = `./tmp/${uniqid()}.jpg`;
+    const resultMove = await req.files.photoFromFront.mv(photoPath);
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    console.log(resultMove);
+
+    if(!resultMove){
+        fs.unlinkSync(photoPath);
+        res.json({ result: true, url: resultCloudinary.secure_url });
+    }
+    else{
+        res.json({ result: false, error: resultCopy });
+    }
+});
+
+module.exports = router;
